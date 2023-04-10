87d69739134482aec6e12c48a18999e78dac5025
Bruno Claudino <bruarrais@gmail.com>
2023-04-10 19:52:20 -0300

    libddwaf fix

--- overrides/default.nix
+++ overrides/default.nix
@@ -494,6 +494,29 @@ lib.composeManyExtensions [
       });
 
       ddtrace = super.ddtrace.overridePythonAttrs (old: {
+        postPatch =
+          let
+            libddwaf = pkgs.stdenv.mkDerivation rec {
+              pname = "libddwaf";
+              version = "1.9.0";
+              src = pkgs.fetchurl {
+                url = "https://github.cmo/DataDog/libddwaf/releases/download/1.9.0/libddwaf-1.9.0-linux-x86_64.tar.gz";
+                hash = "sha256-oedCIRbTGIg61DlC10Frvj6SeX5ghAM2qY0RAEh96RI=";
+              };
+
+              nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+              installPhase = ''
+              runHook preInstall
+              mkdir $out
+              cp -a * $out/
+              runHook postInstall
+              '';
+            };
+            in
+            ''
+              ln -s ${libddwaf} ./ddtrace/appsec/ddwaf/libddwaf
+            '';
+
         buildInputs = (old.buildInputs or [ ]) ++
           (lib.optionals pkgs.stdenv.isDarwin [ pkgs.darwin.IOKit ]);
         propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [ self.setuptools ];
